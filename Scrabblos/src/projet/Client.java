43e909be38ba1aee18adb057a70eea4618c18095
Ning GUO <ai535463858@gmail.com>
2019-10-08 12:50:16 +0200

    essai client

--- Scrabblos/src/projet/Client.java
+++ Scrabblos/src/projet/Client.java
@@ -1,6 +1,7 @@
 package projet;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -8,28 +9,44 @@ import java.net.UnknownHostException;
 import org.json.JSONObject;
 
 public class Client {
-
-	public static void main(String[] args) {
-
-
+	
+	Socket s ;
+	public void connexion() {
 
 		JSONObject object = new JSONObject();
 		object.put("lettre", 'A');
 		System.out.print("debut");
 		try {
-			Socket s = new Socket("127.0.0.1",12345);
+			 s = new Socket("127.0.0.1",12345);
 			OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream());
 //			out.write(object.toString()+"\n");
 			out.write("bonjour\n");
 
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public String getLettreDepart() {
+		try {
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			in.read();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+		Client client = new Client();
+		client.connexion();
+
 	}
 
 }
