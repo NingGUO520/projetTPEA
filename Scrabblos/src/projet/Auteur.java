dbb0f5d899c44940f609740a9eac98d94b458a19
Ning GUO <ai535463858@gmail.com>
2019-10-29 17:06:34 +0100

    avec exemple reussi faut refaire la signature

--- Scrabblos/src/projet/Auteur.java
+++ Scrabblos/src/projet/Auteur.java
@@ -49,14 +49,23 @@ public class Auteur implements Runnable{
 	    return (i&0xff)<<24 | (i&0xff00)<<8 | (i&0xff0000)>>8 | (i>>24)&0xff;
 	}
 	
+	public void read() throws IOException {
+		long taille_ans = inchan.readLong();
+		byte [] cbuf = new byte[(int)taille_ans];
+		inchan.read(cbuf, 0, (int)taille_ans);
+		String s = new String(cbuf,"UTF-8");
+		System.out.println(s);
+		
+	}
 	public boolean register() throws IOException, ClassNotFoundException, NoSuchAlgorithmException {
 		JSONObject obj = new JSONObject();
 		String [] key = pair.getPublic().toString().split("\n");
-		
+
 //		keyPublic = "8f7935d9b9aae9bfabed887acf4951b6f32ec59e3baf3718e8eac4961f3efd36";
 		keyPublic = sha256("auteur1");
-		System.out.println("keyPublic = "+keyPublic);
-		obj.put("register",keyPublic );
+//		System.out.println("keyPublic = "+keyPublic);
+		obj.put("register","b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f" );
+//		obj.put("register",keyPublic);
 		String msg = obj.toString();
 		long taille = msg.length();
 		outchan.writeLong(taille);
@@ -64,9 +73,6 @@ public class Auteur implements Runnable{
 		long taille_ans = inchan.readLong();
 		byte [] cbuf = new byte[(int)taille_ans];
 		inchan.read(cbuf, 0, (int)taille_ans);
-//		System.out.println(new String(cbuf,"UTF-8"));
-		
-		
 		String s = new String(cbuf,"UTF-8");
 		System.out.println(s);
 		
@@ -77,37 +83,45 @@ public class Auteur implements Runnable{
 			String x = (String)a;
 			letters.add(x);
 		}
-//		inchan.read();
 		return true;
 	}
 	
 	public boolean injectLettre() throws IOException, NoSuchAlgorithmException {
-		JSONObject obj = new JSONObject();
-		String a = letters.get(3);
-
+		JSONObject letter = new JSONObject();
+//		String a = letters.get(3);
+		String a = "a";
 		String head = sha256("");
-//		String signature = sha256(a+periode+head);
 		
-//		String auteur = "b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f";
-//		signature+=sha256(keyPublic);
-		System.out.println("head = " +head);
-//		String signature = "8b6547447108e11c0092c95e460d70f367bc137d5f89c626642e1e5f2ceb6108043d4a080223b467bb810c52b5975960eea96a2203a877f32bbd6c4dac16ec07";
+		String auteur = "b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f";
+//		System.out.println("head = " +head);
+		String signature = "8b6547447108e11c0092c95e460d70f367bc137d5f89c626642e1e5f2ceb6108043d4a080223b467bb810c52b5975960eea96a2203a877f32bbd6c4dac16ec07";
 	
-		String signature = sha512(a+periode+head+keyPublic);
-		System.out.println("signature = " +signature);
-//		System.out.println("auteur = " +auteur);
-		System.out.println("periode = " +periode);
+//		String signature = sha512(a+periode+head+keyPublic);
+//		System.out.println("signature = " +signature);
+//		System.out.println("periode = " +periode);
 
-		obj.put("letter", a);
-		obj.put("periode", periode);
-		obj.put("head", head);
-		obj.put("author", keyPublic);
-		obj.put("signature", signature);
+		letter.put("letter", a);
+		letter.put("period", periode);
+		letter.put("head", head);
+		letter.put("author", auteur);
+		letter.put("signature", signature);
+		
+		
+		JSONObject obj = new JSONObject();
+		obj.put("inject_letter", letter);
+		
+//		obj.put("inject_letter", "{ \"letter\":\"a\", \"period\":0,\n" + 
+//				"     \"head\":\"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\",\n" + 
+//				"     \"author\":\"b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f\",\n" + 
+//				"     \"signature\":\"8b6547447108e11c0092c95e460d70f367bc137d5f89c626642e1e5f2ceb6108043d4a080223b467bb810c52b5975960eea96a2203a877f32bbd6c4dac16ec07\"\n" + 
+//				"   }\n" + 
+//				"");
+
 		
 		String msg = obj.toString();
 		long taille = msg.length();
 		outchan.writeLong(taille);
-		outchan.writeUTF(msg);
+		outchan.write(msg.getBytes("UTF-8"),0,(int)taille);
 		
 		periode++;
 		return true;
@@ -147,38 +161,67 @@ public class Auteur implements Runnable{
 		   return hash512(base);
 		}
 	
-	public boolean getLetter_pool() throws IOException {
+	public boolean ecouteContinue() throws IOException {
 		JSONObject obj = new JSONObject();
-		obj.put("listen","null");
+		obj.put("listen",JSONObject.NULL);
 		String msg = obj.toString();
 		long taille = msg.length();
 		outchan.writeLong(taille);
-		outchan.writeUTF(msg);
+		outchan.write(msg.getBytes("UTF-8"),0,(int)taille);
 		return true;
 		
 	}
+	
+	public boolean getFullLetterPool() throws IOException {
+		JSONObject obj = new JSONObject();
+		obj.put("get_full_letterpool",JSONObject.NULL);
+		String msg = obj.toString();
+		long taille = msg.length();
+		outchan.writeLong(taille);
+		outchan.write(msg.getBytes("UTF-8"),0,(int)taille);
+		
+		read();
+		return true;
+	}
+	
+	
+	public boolean getLetterPoolSince(int p) throws IOException {
+		JSONObject obj = new JSONObject();
+		obj.put("get_letterpool_since",p);
+		String msg = obj.toString();
+		long taille = msg.length();
+		outchan.writeLong(taille);
+		outchan.write(msg.getBytes("UTF-8"),0,(int)taille);
+		read();
+
+		return true;
+	}
+	
+	
 
 	@Override
 	public void run() {
 		try {
-			try {
+				
 				register();
 				System.out.println("fin de registration");
 				injectLettre();
 				System.out.println("fin de envoyer de lettre");
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//				ecouteContinue();
+//
+				getFullLetterPool();
+				getLetterPoolSince(0);
 			
-
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 	}
 }
--- Scrabblos/src/projet/MainTest.java
+++ Scrabblos/src/projet/MainTest.java
@@ -6,12 +6,21 @@ import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
 
 public class MainTest {
-	public static void main (String [] args) throws IOException, NoSuchAlgorithmException, InterruptedException {
+	public static void main (String [] args) throws IOException, NoSuchAlgorithmException, InterruptedException, ClassNotFoundException {
 //
 		Socket s = new Socket("127.0.0.1", 12345);
 		Auteur aut = new Auteur(s);
+		
 		Thread t = new Thread(aut);
 		t.start();
+//		aut.ecouteContinue();
+//		aut.register();
+//		aut.injectLettre();
+//		aut.getFullLetterPool();
+//		aut.getLetterPoolSince(0);
+		
+//		
+	
 //		
 //		Politicien pol = new Politicien("127.0.0.1", 12345);
 //		Thread t1 = new Thread(pol);
